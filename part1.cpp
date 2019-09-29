69b5867082ec5167479ac6a1622406f57c2798b1
dylanchristopherson <dchristropherson@gmail.com>
2019-09-29 18:34:34 -0500

    Beginning attempts at pipes

--- part1.cpp
+++ part1.cpp
@@ -1,19 +1,38 @@
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 #include <iostream>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <stdlib.h>
 
 using namespace std;
 
-
 int main()
 {
-
+ size_t Maxsize=80;
+ int fd[2],nbytes; 
+ char sharedStr[]="Hello, CS442!\n";
+ char readbuffer[80];
  pid_t pid;
+
+ if(pipe(fd)<0) exit(1);
+
  pid = fork();
 
  if(pid == 0)
  {
   cout << "A New Child was created \n";
+
+  for(int i = 0; i < 2; i++) {
+	cout << "TEST: " << i << endl;
+  }
+
+ close(fd[0]);
+
+ write(fd[1], sharedStr, Maxsize);
+ exit(0);
+
  }
  else if (pid < 0)
  {
@@ -22,7 +41,17 @@ int main()
  }
  else
  {
-  cout << "I am the parent :) \n";
+	wait(0);
+       	close(fd[1]);
+
+	nbytes=read(fd[0], readbuffer,sizeof(readbuffer));
+	
+	cout << "Readbuffer: " << readbuffer << endl;	
+	
+	
+	cout << "I am the parent :) \n";
+
+
   return 0;
  }
 
