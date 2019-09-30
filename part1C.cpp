4a19de757a7ee39ec8ae1e482eafe5ae8fcec57d
mattsonb2465 <mattsonb2465@my.uwstout.edu>
2019-09-30 15:53:39 -0500

    nice

---
+++ part1C.cpp
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <iostream>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <stdlib.h>
+
+using namespace std;
+
+int main()
+{
+	size_t Maxsize=80;
+ 	int fd[2],nbytes; 
+ 	char sharedStr1[]="Your full name";
+	char sharedStr2[]="your department";
+	char sharedStr3[]="add your email address";
+ 	char readbuffer[80];
+	pid_t pid;
+
+ 	if(pipe(fd)<0) exit(1);
+
+ 	pid = fork();
+
+	if(pid == 0)
+ 	{
+		cout << "A New Child was created \n";
+
+		for(int i = 0; i < 2; i++) {
+		cout << "TEST: " << i << endl;
+		}
+
+		close(fd[0]);
+
+		write(fd[1], sharedStr1, Maxsize);
+		write(fd[1], sharedStr2, Maxsize);
+		write(fd[1], sharedStr3, Maxsize);
+		exit(0);
+
+	}
+	else if (pid < 0)
+	{
+		cout << "No new child was created \n";
+		return 0;
+	}
+	else
+	{
+		wait(0);
+       		close(fd[1]);
+		cout << "~in the parent process~" << endl;
+		nbytes=read(fd[0], readbuffer,sizeof(readbuffer));
+		cout << readbuffer << endl;
+		nbytes=read(fd[0], readbuffer,sizeof(readbuffer));
+		cout << readbuffer << endl;
+		nbytes=read(fd[0], readbuffer, sizeof(readbuffer));
+		cout << readbuffer << endl;
+
+		cout << "Readbuffer: " << readbuffer << endl;
+
+
+		return 0;
+	}
+
+return 0;
+}
