186e3a60658c55d996f92995693f696d233615c7
dylanchristopherson <dchristropherson@gmail.com>
2019-10-02 15:26:51 -0500

    Added in reading lines from file and the use of history and pwd

--- part1D.cpp
+++ part1D.cpp
@@ -7,18 +7,65 @@
 
 using namespace std;
 
-int main() {
+void getPwd() {
 	// Part 1
 	size_t Maxsize = 80;	
 	char Readbuffer[80];
 
-
 	getcwd(Readbuffer, Maxsize);
 
 	cout << Readbuffer << endl;
+}
 
-	// Part 2
+void getHistory(vector<string> v) {
+	for (int i = 0; i < v.size(); i++) {
+		cout << i+1 << " " << v[i] << endl; 
+	}
+}
+
+int main() {
+
+	string str;
+	cout << "The command line shout repeat now\n";
+
 	vector<string> v;
+
+	char * cstr;
+	char * pch;
+	char *C;
+
+	while(1) {
+		getline(cin,str);
+
+		cstr = new char [str.length()+1];
+		strcpy(cstr, str.c_str());
+
+		string a;
+	        pch = strtok (cstr, " \t\n");
+        
+		a += pch;
+
+		while(pch != NULL) {
+//			printf( " %s\n", pch);
+
+			pch = strtok(NULL, " \t\n");
+			if (pch != NULL) a += pch;
+		}
+	 	const char *C = a.c_str();
+	
+		v.push_back(cstr);
+
+		if (strcmp(C, "pwd") == 0) getPwd();
+		if (strcmp(C, "history") == 0) getHistory(v);
+
+	}
+
+	delete pch;
+	delete[] cstr;
+	return(0);
+}
+/*
+// Part 2
 
 	v.push_back("Test");
 	v.push_back("Hello again");
@@ -33,6 +80,5 @@ int main() {
 	//for (int i = 0; i < hist; i++) {
 	//	cout << "Vector: " << v[i] << endl;
 	//}
+*/
 
-	return(0);
-}
