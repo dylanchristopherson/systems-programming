08b8ed8ffaf2f2d3d215acce561bdd39a76f4c99
dylanchristopherson <dchristropherson@gmail.com>
2019-11-05 15:30:07 -0600

    Commiting work so that I can try it from outside my vm, I might be low on mem

--- assignment_5/cpp/main.cpp
+++ assignment_5/cpp/main.cpp
@@ -11,10 +11,23 @@ class Customer {
 		string id;
 		double balance;
 	
+		Customer() {
+			id = "";
+			balance = 0.0;
+		}
+
 		Customer(string ID, double BALANCE) {
 			id = ID;
 			balance = BALANCE;
 		} 
+
+		void setID(string ID) {
+			id = ID;
+		}
+
+		void setBalance(double BALANCE) {
+			balance = BALANCE;
+		}
 
 		string getID() {
 			return id;
@@ -28,6 +41,11 @@ class Customer {
 
 int main () {
 
+	// There are 8000000 lines in the file, that means there
+	// are 4000000 customers
+
+	Customer custList[4000];
+
 	int time = 0;
 	cout << "Hello world\n";
 
@@ -35,6 +53,38 @@ int main () {
 
 	cout << "John id: " << john.getID() << endl;
 	cout << "John balance: " << john.getBalance() << endl;
+
+	ifstream inFile;
+	inFile.open("./accounts.txt");
+
+	if (!inFile) {
+		cerr << "Unable to open file accounts.txt" << endl;
+		exit(1);
+	}
+/*
+	string line;
+	int number_of_lines;
+	while (std::getline(inFile, line))
+	        ++number_of_lines;
+	std::cout << "Number of lines in text file: " << number_of_lines;
+*/
+
+	string id;
+	double balance;
+	int count = 0;
+	while (inFile >> id) {
+		inFile >> balance;
+		cout << "id: " << id << endl;
+		cout << "balance: " << balance << endl;  
+
+		custList[count].setID(id);
+		custList[count].setBalance(balance);
+		count ++;
+
+	} 
+
+	// custList[0].setID("TEST");
+	// custList[0].setBalance(100);
 
 	// Create Arraylist of customers
 
